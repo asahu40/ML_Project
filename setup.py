5a057d3410e1e9af220c6c43ee83a02cfc2cfe5b
Sahu <amit.sahu@suez.com>
2022-11-23 16:40:30 +0530

    Updated Housing Folder and setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from typing import List
+
+
+## Declaring Variables for setup file
+Project_Name = "housing-Predictor"
+Version = "0.0.1"
+Author = "Amit Kumar Sahu"
+Description = "This is FSDS Nov Batch first Machine Learning Project(End to End"
+Folder = ["housing"]
+Requirement_File = "requirements.txt"
+
+def get_requirements_list()->List(str):
+    """
+    Description : This Function is going to return a list of  requirements mention in requirements.txt file
+    Return : This function will return a list contains in requirements.txt file 
+    
+    """
+    with open(Requirement_File) as requirement_file :
+        return requirement_file.readlines()
+
+setup(name=Project_Name,
+version=Version,
+author=Author,
+description=Description,
+packages=Folder,
+install_requires = get_requirements_list()
+)
