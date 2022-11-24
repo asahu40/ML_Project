9823c4e6717b43091693731cbd7c2cdada66f445
Sahu <amit.sahu@suez.com>
2022-11-24 12:00:39 +0530

    Updated Setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup , find_packages
 from typing import List
 
 
@@ -7,10 +7,10 @@ Project_Name = "housing-Predictor"
 Version = "0.0.1"
 Author = "Amit Kumar Sahu"
 Description = "This is FSDS Nov Batch first Machine Learning Project(End to End"
-Folder = ["housing"]
+#Folder = ["housing"]
 Requirement_File = "requirements.txt"
 
-def get_requirements_list()->List(str):
+def get_requirements_list():
     """
     Description : This Function is going to return a list of  requirements mention in requirements.txt file
     Return : This function will return a list contains in requirements.txt file 
@@ -19,10 +19,11 @@ def get_requirements_list()->List(str):
     with open(Requirement_File) as requirement_file :
         return requirement_file.readlines()
 
-setup(name=Project_Name,
+setup(
+name=Project_Name,
 version=Version,
 author=Author,
 description=Description,
-packages=Folder,
+packages=find_packages(), ## In this line we can add find_packages() instead of Folder Variable 
 install_requires = get_requirements_list()
 )
