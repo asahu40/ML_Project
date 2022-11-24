b435b79e1af96b19e0c4b1d997296278f1ba0e57
Sahu <amit.sahu@suez.com>
2022-11-24 14:55:56 +0530

    Floder Structure Completed

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ Project_Name = "housing-Predictor"
 Version = "0.0.1"
 Author = "Amit Kumar Sahu"
 Description = "This is FSDS Nov Batch first Machine Learning Project(End to End"
-#Folder = ["housing"]
+##Folder = ["housing"]
 Requirement_File = "requirements.txt"
 
-def get_requirements_list():
+def get_requirements_list()->List[str]:
     """
     Description : This Function is going to return a list of  requirements mention in requirements.txt file
     Return : This function will return a list contains in requirements.txt file 
