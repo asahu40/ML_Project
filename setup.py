0a96283c33bb9a6f01a192f1c4ac96d3e408935f
Sahu <amit.sahu@suez.com>
2022-11-25 16:40:32 +0530

    Requirements Erro Fixed

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ Version = "0.0.1"
 Author = "Amit Kumar Sahu"
 Description = "This is FSDS Nov Batch first Machine Learning Project(End to End"
 ##Folder = ["housing"]
-Requirement_File = "requirements.txt"
+##Requirement_File = "requirements.txt"
+Requirement_File = r"E:\Study Material\Data Science\INeuron\Full Stack Data Science\Projects\Machine Learning Project (End to End )\INeuron_ML_Project_1\ML_Project\requirements.txt"
 
-def get_requirements_list() -> List[str]:
+def get_requirements_list() -> list[str]:
     """
     Description : This Function is going to return a list of  requirements mention in requirements.txt file
     Return : This function will return a list contains in requirements.txt file 
     
     """
     with open(Requirement_File) as requirement_file :
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name=Project_Name,
