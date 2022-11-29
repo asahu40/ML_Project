34a8fb2f46848971446ed103f05f72986ca23042
Sahu <amit.sahu@suez.com>
2022-11-29 12:22:01 +0530

    Study Folder added

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 
 Log_Dir = "housing_logs"
-Current_Time_Stamp = "f{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+Current_Time_Stamp = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 Log_File_Name = f"log_{Current_Time_Stamp}.log"
 os.makedirs(Log_Dir,exist_ok=True)
 Log_File_Path = os.path.join(Log_Dir,Log_File_Name)
