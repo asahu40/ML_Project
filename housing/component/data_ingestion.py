db5dcb9fc8e888e9e93205ec848de8a7829c8bae
Sahu <amit.sahu@suez.com>
2022-12-09 21:00:39 +0530

    Updated

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -2,6 +2,11 @@ from housing.entity.config_entity import DataIngestionConfig
 from housing.logger import logging
 from housing.exception import HousingException
 import sys ,os
+from housing.entity.artifact_entity import DataIngestionArtifact
+import tarfile
+from six.moves import urllib
+import pandas as pd
+
 
 
 class DataIngestion():
@@ -13,12 +18,75 @@ class DataIngestion():
         except Exception as e :
             raise HousingException (e ,sys) from e
 
-
     
-    def initiate_data_ingestion(self) -> DataIngestionArtifact :
+    def download_housing_data(self) -> str:
         try :
-            pass
-        except :
+            ## Extracttion of remote url to download
+            download_url = self.data_ingestion_config.dataset_download_url
+
+            ## Folder Location to download file
+            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+
+            if os.path.exists(tgz_download_dir):
+                os.remove(tgz_download_dir)
+
+            os.makedirs(tgz_download_dir,exist_ok=True)
+
+            housing_file_name = os.path.basename(download_url)
+
+            tgz_file_path = os.path.join(tgz_download_dir,housing_file_name)
+
+            logging.info(f"Downloading file from : [{download_url}] into : [{tgz_file_path}]")
+
+            urllib.request.urlretrieve(download_url , tgz_file_path)
+
+            logging.info(f"Successfully Donwloaded File : [{tgz_file_path}]")
+
+            return tgz_file_path
+
+        except Exception as e :
+            raise HousingException (e , sys) from e 
+ 
+    def extract_tgz_data(self,tgz_file_path -> str):
+        try :
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+            
+            os.makedirs(raw_data_dir,exist_ok=True)
+
+            logging.info(f"Extracting tgz file :[{tgz_file_path}] into dir : [{raw_data_dir}]")
+
+            with tarfile.open(tgz_file_path) as housing_tgz_file_obj:
+                housing_tgz_file_obj.extractall(path=raw_data_dir)
+
+            logging.info(f"Exrtaction Completed")
+
+
+        except Exception as e :
+            raise HousingException ( e ,sys) from e 
+
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try :
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            file_name = os.listdir(raw_data_dir)[0]
+
+            housing_file_path = os.path.join(raw_data_dir,file_name)
+
+            housing_file_name = pd.read_csv(housing_file_path)
+
+        except Exception as e :
+            raise HousingException (e,sys) from e 
+
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
+        try :
+            tgz_file_path = self.download_housing_data()
+            self.extract_tgz_data(tgz_file_path = tgz_file_path)
+
+        except Exception as e :
             raise HousingException ( e, sys) from e 
 
     
