5b696ab008f2cf02a60e657a75d05d5511449129
Sahu <amit.sahu@suez.com>
2023-01-03 16:02:28 +0530

    updated

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,11 +1,12 @@
 from housing.config.configuration import Configuration
 from housing.logger import logging
 from housing.exception import HousingException
-from housing.entity.config_entity import DataIngestionConfig
-from housing.entity.artifact_entity import DataIngestionArtifact , DataValidationArtifact,DataTransformationArtifact
+from housing.entity.config_entity import DataIngestionConfig,ModelTrainerConfig
+from housing.entity.artifact_entity import DataIngestionArtifact , DataValidationArtifact,DataTransformationArtifact,ModelTrainerArtifact
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
+from housing.component.model_trainer import ModelTrainer
 import os,sys
 
 class Pipeline :
@@ -30,7 +31,7 @@ class Pipeline :
         try :
             data_validation = DataValidation(data_validation_config=self.config.get_data_validation_config() ,
                                              data_ingestion_artifact=data_ingestion_artifact 
-            )
+                                            )
 
             return data_validation.initiate_data_validation()
 
@@ -43,16 +44,22 @@ class Pipeline :
         try :
             data_transformation = DataTransformation(data_transformation_config=self.config.get_data_transformation_config(),
                                                      data_ingestion_artifact=data_ingestion_artifact,
-                                                     data_validation_artifact=data_validation_artifact)
+                                                     data_validation_artifact=data_validation_artifact
+                                                    )
             
             return data_transformation.initiate_data_transformation()
 
         except Exception as e :
             raise HousingException (e,sys) from e
         
-    def start_model_trainer(self):
+    def start_model_trainer(self,data_transformation_artifact : DataTransformationArtifact) -> ModelTrainerArtifact:
         try :
-            pass
+            model_trainer = ModelTrainer(model_trainer_config = self.config.get_model_trainer_config(),
+                                         data_transformation_artifact = data_transformation_artifact
+                                        )
+
+            return model_trainer.initiate_model_trainer()
+            
         except Exception as e :
             raise HousingException (e,sys) from e
 
@@ -75,6 +82,8 @@ class Pipeline :
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_tranformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
                                                                          data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_tranformation_artifact)
+                                                             
             self.run_pipeline()
 
         except Exception as e :
